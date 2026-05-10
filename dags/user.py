01d5ffa44593456a2a4b3752b879f84c32ed4f23
lhduc02 <mcrc1894@gmail.com>
2026-05-10 20:50:43 +0700

    feat: update code DAG using assets

---
+++ dags/user.py
@@ -0,0 +1,35 @@
+import requests
+from airflow.sdk import asset, Asset, Context
+
+@asset(
+    schedule="@daily",
+    uri="https://randomuser.me/api/"
+)
+def user(self) -> dict[str]:
+    r = requests.get(self.uri)
+    return r.json()
+
+
+@asset(
+    schedule=user
+)
+def user_location(user: Asset, context: Context) -> dict[str]:
+    user_data = context['ti'].xcom_pull(
+        dag_id=user.name,
+        task_ids=user.name,
+        include_prior_dates=True,
+    )
+    return user_data['results'][0]['location']
+
+
+
+@asset(
+    schedule=user
+)
+def user_login(user: Asset, context: Context) -> dict[str]:
+    user_data = context['ti'].xcom_pull(
+        dag_id=user.name,
+        task_ids=user.name,
+        include_prior_dates=True,
+    )
+    return user_data['results'][0]['login']
