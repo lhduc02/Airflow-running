58f12fedb27667f56fd3921f31b35c61f17a1f8a
lhduc02 <mcrc1894@gmail.com>
2026-07-11 21:51:31 +0700

    chore add logic push data to BQ in user_processing DAG

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -2,6 +2,7 @@ import csv
 from datetime import datetime
 from airflow.sdk import dag, task
 from airflow.sdk.bases.sensor import PokeReturnValue
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 
 
 @dag(
@@ -39,21 +40,44 @@ def user_processing():
             "id": fake_users["id"],
             "firstname": fake_users["personalInfo"]["firstName"],
             "lastname": fake_users["personalInfo"]["lastName"],
-            "email": fake_users["personalInfo"]["email"],
-            
+            "email": fake_users["personalInfo"]["email"]
         }
     
     @task
     def process_user(user_info):
+        user_info["loaded_at"] - datetime.now().strftime("%Y-%M-%D %H:%M%S")
+        
         with open("/tmp/user_info.csv", "w", newline="") as f:
             writer = csv.DictWriter(f, fieldnames=user_info.keys())
             writer.writeheader()
             writer.writerow(user_info)
         print("Write success")
     
+    @task
+    def store_user():
+        hook = BigQueryHook(gcp_conn_id = "google_cloud_default")
+        
+        local_file_path = "/tmp/user_info.csv"
+        project_id = hook.project_id
+        dataset_id = 'raw_data'
+        table_id = 'user_info'
+
+        # Sử dụng run_load để đẩy file local
+        hook.run_load(
+            destination_project_dataset_table=f"{project_id}.{dataset_id}.{table_id}",
+            source_format='CSV',
+            autodetect=True,
+            write_disposition='WRITE_APPEND',
+            skip_leading_rows=1,
+            src_data=local_file_path
+        )
+    
     fake_user = is_api_available()
     user_info = extract_user(fake_user)
     process_user(user_info)
+    store_user()
+    
+    # create_table >> process_user(extract_user(is_api_available())) >> store_user()
 
 user_processing()
 
